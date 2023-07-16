d42718c69cf9342d6e331e113216b6d24da0160a
kdrcnuzmcu <kdrcn.uzmcu@gmail.com>
2023-07-16 23:48:33 +0300

    Updated with PEP8

--- dosya-islemleri.py
+++ dosya-islemleri.py
@@ -1,10 +1,19 @@
-path = "text files/What is Data Science.txt"
+from re import findall
 
-def KelimeSay(path, word):
-    from re import findall
+file_path = "text files/What is Data Science.txt"
 
+
+def kelime_say(path, word):
+    """
+    This function counts a word in a text.
+    Parameters
+    ----------
+    path: string
+    word: string
+    """
     with open(path, "r", encoding="utf8") as text:
         content = text.read()
         print(len(findall(word, content)))
 
-KelimeSay(path, "are")
+
+kelime_say(file_path, "are")
